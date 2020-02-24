88f4498ce8a1f9f63387d6546aa892d38f256e40
keksik77 <lukashevich7tim@gmail.com>
2020-02-24 03:03:40 +0300

    feat: complet transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,33 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw new 'Error';
+    if (arr.length == 0) return []; 
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') 
+        {
+            i++;
+        } else 
+        if (arr[i] === '--discard-prev') 
+        {
+            result.pop();
+        } else 
+        if (arr[i] === '--double-next') 
+        {
+            if ((i + 1) < arr.length) {
+                result.push(arr[i + 1]);
+            }
+        } else if (arr[i] === '--double-prev') 
+        {
+            if ((i - 1) >= 0) 
+            {
+                result.push(arr[i - 1]);
+            }
+        } else 
+        {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 };
