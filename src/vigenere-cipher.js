c87e7d60f86f96662387e1267b69379a685416ef
keksik77 <lukashevich7tim@gmail.com>
2020-02-24 22:10:51 +0300

    completed vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(bool = true) {
+        this.bool = bool;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        let spaces = 0;
+        if (message === undefined || key === undefined) throw Error;
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        const encrypted = message.map((value, index) => {
+            if (value === " " || /[!@#$(),.\/|*\-&^%:'\d]/.test(value)) {
+                spaces++;
+                return value;
+            }
+            let kIndex;
+            if(index < key.length-1){
+                kIndex = index-spaces;
+            }else
+            {  kIndex = (index-spaces) % key.length ;
+            } 
+            let crChar = value.charCodeAt(0) - 65 + key[kIndex].charCodeAt(0)-65;
+            if (crChar > 25) crChar -= 26;
+            return String.fromCharCode(crChar + 65);
+            }
+        );
+        if (!this.bool) encrypted.reverse();
+        return encrypted.join('');
     }
+
+    decrypt(encryptedMessage, key) 
+    {
+        let spaces = 0;
+        if (encryptedMessage === undefined || key === undefined) throw Error;
+        let message = encryptedMessage.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        const decrypted = message.map((value, index) => {
+                if (value === " " || /[!@#$(),.\/|*\-&^%:'\d]/.test(value)) 
+                {
+                    spaces++;
+                    return value;
+                }
+                let kIndex;
+                if(index < key.length-1){
+                    kIndex = index-spaces;
+                }else
+                {  
+                    kIndex = (index-spaces) % key.length ;
+                } 
+                let crChar = (value.charCodeAt(0) - 65) - (key[kIndex].charCodeAt(0) - 65);
+                if (crChar < 0) crChar += 26;
+                return String.fromCharCode(crChar + 65);
+            }
+        );
+        if (!this.bool) decrypted.reverse();
+        return decrypted.join('');
+    }
+
 }
 
 module.exports = VigenereCipheringMachine;
